61fa55d7be468d5b39d13afe8806a87748ccdced
asinaga117 <asinaga1994@gmail.com>
2022-04-27 10:24:56 +0000

    [add]admin_ログイン画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  #管理者用
+  devise_for :admins,skip: [:resistrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  #顧客用
+  devise_for :customers,skip: [:passwords], controllers: {
+    resistrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  root to: 'homes#top'
+  get 'homes/about' => 'homes#about',as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
