76f62bf6af3a99205282d9247dfab738b913c3a9
asinaga117 <asinaga1994@gmail.com>
2022-05-03 02:08:25 +0000

    [ADD]商品一覧編集画面の追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,21 @@ Rails.application.routes.draw do
   devise_for :admins,skip: [:resistrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  
+
   #顧客用
   devise_for :customers,skip: [:passwords], controllers: {
     resistrations: "public/registrations",
     sessions: 'public/sessions'
   }
+
   root to: 'homes#top'
   get 'homes/about' => 'homes#about',as: 'about'
+
+  namespace :admin do
+  resources :genres, only: [:index, :create, :edit, :update]
+
+  resources :items, only: [:index, :new, :create, :show, :edit, :update]
+end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_25_120320) do
+ActiveRecord::Schema.define(version: 2022_05_01_001135) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -73,6 +73,22 @@ ActiveRecord::Schema.define(version: 2022_04_25_120320) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer "genre_id"
+    t.string "name"
+    t.text "introduction"
+    t.integer "price"
+    t.boolean "is_active"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
